db9c294fe8af7ca06daa63d85ffa26935574c796
SI-Tanbir <si017490@gmail.com>
2024-11-12 23:20:41 +0600

    adding grid here

--- src/countries/Countries.jsx
+++ src/countries/Countries.jsx
@@ -1,17 +1,19 @@
 import Country from "../country/Country"
+import './Countries.css'
 
 export default function Countries({country}){
 //  console.log(country)
 
     return(
         <div>
+            <h3>list of Country:{country.length}</h3>
+            <div className="Country-container">
+{
+    country.map((result,index) => (    <Country key={index} country={result}></Country>))
+}
+            </div>
 
-        <h3>list of Country:{country.length}</h3>
-
-
-        {
-            country.map((result,index) => (    <Country key={index} country={result}></Country>))
-        }
+       
 
         </div>
     )
--- src/country/Country.jsx
+++ src/country/Country.jsx
@@ -1,12 +1,24 @@
 export default function Country({ country }) {
-    const { name, languages, area } = country;
-    console.log(languages); // Log languages to see the structure
+    const { name, languages, area,flags } = country;
+    console.log(country); // Log languages to see the structure
   
     return (
-      <div>
+      <div style={{border:'1px solid grey',
+        margin:'10px',
+        padding:'10px',
+        borderRadius:'10px',
+        textAlign:'center'
+      }}>
+
         <h3>Name: {name.common}</h3>
+        <img src={flags.png} alt=""  width='200px'/>
         <h4>Area: {area} km²</h4>
-        <h4>Languages:</h4>
+       
+       <div style={{display:'flex',
+        justifyContent:'center'
+       }}>
+
+       <h4>Languages:</h4>
         
         <ul>
     
@@ -15,6 +27,7 @@ export default function Country({ country }) {
               <li key={index}>{language}</li> 
             ))}
         </ul>
+       </div>
       </div>
     );
   }
