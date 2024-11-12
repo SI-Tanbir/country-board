4535696bbc5d8af9be44dae62f50de2a464d06bf
SI-Tanbir <si017490@gmail.com>
2024-11-12 21:10:15 +0600

    after huge jugggled finnaly make it

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Countries from './countries/Countries'
+import { useEffect, useState } from 'react'
+import Countries from './countries/Countries';
+
 
 function App() {
+
+
+  let [country,setCountry]=useState([]);
+
+  useEffect(()=>{
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(data=>{
+      // console.log(data)
+      setCountry(data)
+    }
+      )
+  },[])
  
 
   return (
     <div>
     
      <h1>Country board</h1>
+
+     <Countries country={country}></Countries>
+
+    
+
+
+
      </div>
   )
 }
---
+++ src/countries/Countries.jsx
@@ -0,0 +1,12 @@
+
+export default function Countries({country}){
+ console.log(country)
+
+    return(
+        
+
+        <h3>list of Country:{country.length}</h3>
+        
+
+    )
+}
